617be3d6164ad00ecc06d99fb5fcfa478f748e47
Yahia Mahmoud Abdelrahman Bassyouny <66659379+PricelessCodes@users.noreply.github.com>
2022-12-13 09:25:50 +0200

    Promisifying Http Requests (with XMLHttpRequest)

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,31 +1,46 @@
-// CREATE XML Http Request
-const xhr = new XMLHttpRequest();
-//CONFIQRATE REQUEST
-xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+const listElement = document.querySelector(".posts");
+const postTemplate = document.getElementById("single-post");
 
-//NOT ALL BROWSERS SUPPORT IT SO WE USE ONLOAD
-//xhr.addEventListener();
+function sendHttpRequest(method, url) {
+    const promise = new Promise((resolve, reject) => {
+        // CREATE XML Http Request
+        const xhr = new XMLHttpRequest();
+        //CONFIQRATE REQUEST
+        xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
 
-//tell js that the response of type json to avoid parsing
-xhr.responseType = 'json';
+        //NOT ALL BROWSERS SUPPORT IT SO WE USE ONLOAD
+        //xhr.addEventListener();
 
-//CALLED WHEN REQUEST IS RESPONED
-xhr.onload = function () {
-    //xhr.response Return JSON data so we need to parse it
-    // a better way than parsing is to use response type equal to json
+        //tell js that the response of type json to avoid parsing
+        xhr.responseType = "json";
 
-    const listOfPosts = xhr.response;
+        //CALLED WHEN REQUEST IS RESPONED
+        xhr.onload = function () {
+            //xhr.response Return JSON data so we need to parse it
+            // a better way than parsing is to use response type equal to json
+
+            resolve(xhr.response);
+        };
+
+        //SEND REQUEST
+        xhr.send();
+    });
+
+    return promise;
+}
+
+async function fetchPosts() {
+    const responseData = await sendHttpRequest();
+
+    const listOfPosts = responseData;
 
     for (const post of listOfPosts) {
         const postEl = document.importNode(postTemplate.content, true);
-        postEl.querySelector("h2").textContent = post.title.toUpperCase();
+        postEl.querySelector("h2").textContent =
+            post.title.toUpperCase();
         postEl.querySelector("p").textContent = post.body;
         listElement.append(postEl);
     }
 }
 
-//SEND REQUEST
-xhr.send();
-
-const listElement = document.querySelector(".posts");
-const postTemplate = document.getElementById("single-post");
+fetchPosts();
