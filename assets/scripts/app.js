dec5fec4b7827c7727c363d51d6a546bb65ce9c1
Yahia Mahmoud Abdelrahman Bassyouny <66659379+PricelessCodes@users.noreply.github.com>
2022-12-13 10:43:26 +0200

    Sending a DELETE Request

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,7 @@ const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 const form = document.querySelector("#new-post form");
 const fetchButton = document.querySelector("#available-posts button");
+const postList = document.querySelector("ul");
 
 function sendHttpRequest(method, url, data) {
     const promise = new Promise((resolve, reject) => {
@@ -43,6 +44,7 @@ async function fetchPosts() {
         const postEl = document.importNode(postTemplate.content, true);
         postEl.querySelector("h2").textContent = post.title.toUpperCase();
         postEl.querySelector("p").textContent = post.body;
+        postEl.querySelector("li").id = post.id;
         listElement.append(postEl);
     }
 }
@@ -58,11 +60,20 @@ async function CreatePost(title, content) {
 }
 
 fetchButton.addEventListener("click", fetchPosts);
-form.addEventListener("submit", event => {
+
+form.addEventListener("submit", (event) => {
     event.preventDefault();
 
     const enteredTitle = event.currentTarget.querySelector("#title").value;
     const enteredContent = event.currentTarget.querySelector("#content").value;
 
     CreatePost(enteredTitle, enteredContent);
+});
+
+postList.addEventListener("click", (event) => {
+    if (event.target.tagName === "BUTTON") {
+        const post = event.target.closest("li");
+        sendHttpRequest("DELETE", `https://jsonplaceholder.typicode.com/posts/${post.id}`);
+        postList.removeChild(post);
+    }
 });
